15a35363b080a44da5737a25b20609fedaba2203
Ibrahim <ibrahimjunior67@yahoo.com>
2021-11-25 11:12:51 +0100

    test

--- BookLibraryApplication/Data/DataContext.cs
+++ BookLibraryApplication/Data/DataContext.cs
@@ -16,80 +16,99 @@ namespace BookLibraryApplication.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Book>()
-            //    .HasData(
-            //        new Book
-            //        {
-            //            Id = 1,
-            //            Title = "Joys of Motherhood",
-            //            Description = "About the expectations of the African Woman",
-            //            ISBN = "145866",
-            //            IsFavorite = true,
-            //            CategoryId = 1,
-            //            //AuthorBooks = new List<AuthorBook> { 
-            //            //    new AuthorBook(){BookId = 1, AuthorsId = 1 },
-            //            //}
-            //        },
-            //        new Book
-            //        {
-            //            Id = 2,
-            //            Title = "The 48 Laws of Power",
-            //            Description = "Title speaks for itself",
-            //            ISBN = "45698",
-            //            IsFavorite = true,
-            //            CategoryId = 2,
-            //            //AuthorBooks = new List<AuthorBook> {
-            //            //    new AuthorBook(){BookId = 2, AuthorsId = 2 },
-            //            //}
-            //        },
-            //        new Book
-            //        {
-            //            Id = 3, 
-            //            Title = "Things fall apart",
-            //            ISBN = "",
-            //            IsFavorite = false,
-            //            CategoryId = 1,
-            //            //AuthorBooks = new List<AuthorBook>
-            //            //{
-            //            //    new AuthorBook(){BookId = 3, AuthorsId = 3}
-            //            //}
-            //        }
+            modelBuilder.Entity<Book>()
+                .HasData(
+                    new Book
+                    {
+                        Id = 1,
+                        Title = "Joys of Motherhood",
+                        Description = "About the expectations of the African Woman",
+                        ISBN = "145866",
+                        IsFavorite = true,
+                        CategoryId = 1,
+                        //AuthorBooks = new List<AuthorBook> { 
+                        //    new AuthorBook(){BookId = 1, AuthorsId = 1 },
+                        //}
+                    },
+                    new Book
+                    {
+                        Id = 2,
+                        Title = "The 48 Laws of Power",
+                        Description = "Title speaks for itself",
+                        ISBN = "45698",
+                        IsFavorite = true,
+                        CategoryId = 2,
+                        //AuthorBooks = new List<AuthorBook> {
+                        //    new AuthorBook(){BookId = 2, AuthorsId = 2 },
+                        //}
+                    },
+                    new Book
+                    {
+                        Id = 3,
+                        Title = "Things fall apart",
+                        ISBN = "",
+                        IsFavorite = false,
+                        CategoryId = 1,
+                        //AuthorBooks = new List<AuthorBook>
+                        //{
+                        //    new AuthorBook(){BookId = 3, AuthorsId = 3}
+                        //}
+                    }
 
-            //    );
+                );
 
-            //modelBuilder.Entity<Author>()
-            //    .HasData(
-            //        new Author
-            //        {
-            //            Id = 1,
-            //            AuthorName = "Buchi Emecheta"
-            //        },
-            //        new Author
-            //        {
-            //            Id = 2,
-            //            AuthorName = "Robert Greene"
-            //        },
-            //        new Author
-            //        {
-            //            Id = 3,
-            //            AuthorName = "Chinua Achebe"
-            //        }
+            modelBuilder.Entity<Author>()
+                .HasData(
+                    new Author
+                    {
+                        Id = 1,
+                        AuthorName = "Buchi Emecheta"
+                    },
+                    new Author
+                    {
+                        Id = 2,
+                        AuthorName = "Robert Greene"
+                    },
+                    new Author
+                    {
+                        Id = 3,
+                        AuthorName = "Chinua Achebe"
+                    }
 
-            //    );
+                );
 
-            //modelBuilder.Entity<Category>()
-            //    .HasData(
-            //        new Category
-            //        {
-            //            Id = 1,
-            //            CategoryName = "Fiction"
-            //        },
-            //        new Category
-            //        {
-            //            Id = 2,
-            //            CategoryName = "Biographies & Memoirs"
-            //        }
-            //    );
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    new Category
+                    {
+                        Id = 1,
+                        CategoryName = "Fiction"
+                    },
+                    new Category
+                    {
+                        Id = 2,
+                        CategoryName = "Biographies & Memoirs"
+                    }
+                );
+
+            modelBuilder.Entity<AuthorBook>()
+                .HasData(
+                    new AuthorBook
+                    {
+                        BookId = 1,
+                        AuthorsId = 1
+                    },
+                    new AuthorBook
+                    {
+                        BookId = 2,
+                        AuthorsId = 2
+                    },
+                    new AuthorBook
+                    {
+                        BookId = 3,
+                        AuthorsId = 3
+                    }
+                );
             
 
             modelBuilder.Entity<AuthorBook>()
